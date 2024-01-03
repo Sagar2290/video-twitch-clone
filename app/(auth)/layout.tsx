4026d4c39ec3c5f2037f1c34194103f0dbfcf8d6
Sagar <bhagwatsagar2290@gmail.com>
2024-01-03 17:37:42 +0530

    added clerk, prisma and theme changes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { Logo } from "./_components/logo";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AuthLayout = ({ children }: Props) => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-6">
+      <Logo />
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
