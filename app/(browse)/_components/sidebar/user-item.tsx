76a12ac3dbf8839f9a09a0ef4a737ce8f1e9955c
Sagar <bhagwatsagar2290@gmail.com>
2024-01-08 17:58:48 +0530

    userItem component added

---
+++ app/(browse)/_components/sidebar/user-item.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
+
+interface UserItemProps {
+  username: string;
+  imageUrl: string;
+  isLive?: boolean;
+}
+
+export const UserItem = ({ username, imageUrl, isLive }: UserItemProps) => {
+  const pathName = usePathname();
+
+  const { collapsed } = useSidebar((state) => state);
+
+  const href = `/${username}`;
+  const isActive = pathName === href;
+
+  return (
+    <Button
+      asChild
+      variant="ghost"
+      className={cn(
+        "w-full h-12",
+        collapsed ? "justify-center" : "justify-start",
+        isActive && "bg-accent"
+      )}
+    >
+      <Link href={href}>
+        <div
+          className={cn(
+            "flex items-center w-full gap-x-4",
+            collapsed && "justify-center"
+          )}
+        >
+          UserAvatar
+        </div>
+      </Link>
+    </Button>
+  );
+};
