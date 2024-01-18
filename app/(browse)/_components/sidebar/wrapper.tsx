c3818f3d87f8a9a4b50bf469409008837a14a5fc
Sagar <bhagwatsagar2290@gmail.com>
2024-01-18 10:23:29 +0530

    solved hydration error due to client store mismatch in ssr

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,14 +1,38 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  // useIsClient() is shorter way of checking by using useState() and useEffect() commented below.
+  const isClient = useIsClient();
+  // const [isClient, setIsClient] = useState(false);
   const { collapsed } = useSidebar((state) => state);
+
+  // useEffect(() => {
+  //   setIsClient(true);
+  // }, []);
+
+  if (!isClient) {
+    return (
+      <aside
+        className={
+          "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50"
+        }
+      >
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
+  }
 
   return (
     <aside
